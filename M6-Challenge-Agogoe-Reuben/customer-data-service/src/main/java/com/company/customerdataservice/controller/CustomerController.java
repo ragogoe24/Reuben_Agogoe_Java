d9ed787afcf7c4b9dc97db01ed5d96979411bdfb
Reuben Agogoe <ragogoe@princeton.edu>
2023-07-26 01:19:08 +0000

    Added customer controller tests

--- M6-Challenge-Agogoe-Reuben/customer-data-service/src/main/java/com/company/customerdataservice/controller/CustomerController.java
+++ M6-Challenge-Agogoe-Reuben/customer-data-service/src/main/java/com/company/customerdataservice/controller/CustomerController.java
@@ -14,13 +14,27 @@ public class CustomerController {
     @Autowired
     CustomerRepository repo;
 
+    @GetMapping("/customers")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Customer> getAllCustomers(){
+        System.out.println("here 3");
+
+        return repo.findAll();
+    }
+
     @GetMapping("/customers/state/{name}")
+    @ResponseStatus(HttpStatus.OK)
     public List<Customer> getAllCustomersByState(@PathVariable String name){
+        System.out.println("here 2");
+
         return repo.findAllCustomersByState(name);
     }
 
     @GetMapping("/customers/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Customer getCustomerById(@PathVariable int id){
+        System.out.println("here 1");
+
         Optional<Customer> returnVal = repo.findById(id);
         return returnVal.isPresent() ? returnVal.get() : null;
     }
@@ -28,19 +42,40 @@ public class CustomerController {
     @PostMapping("/customers")
     @ResponseStatus(HttpStatus.CREATED)
     public Customer addCustomer(@RequestBody Customer customer){
+        System.out.println("customer created " + customer);
         return repo.save(customer);
     }
 
-    @PutMapping("/customers")
+    @PutMapping("/customers/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public Customer updateCustomer(@RequestBody Customer customer){
-        return repo.save(customer);
+    public void updateCustomer(@RequestBody Customer customer, @PathVariable int id){
+       Optional<Customer> returnVal =  repo.findById(id);
+       if(returnVal.isPresent()){
+           Customer updatedCustomer = returnVal.get();
+           updatedCustomer.setFirstName(customer.getFirstName());
+           updatedCustomer.setLastName(customer.getLastName());
+           updatedCustomer.setEmail(customer.getEmail());
+           updatedCustomer.setCompany(customer.getCompany());
+           updatedCustomer.setPhone(customer.getPhone());
+           updatedCustomer.setAddress1(customer.getAddress1());
+           updatedCustomer.setAddress2(customer.getAddress2());
+           updatedCustomer.setCity(customer.getCity());
+           updatedCustomer.setState(customer.getState());
+           updatedCustomer.setPostalCode(customer.getPostalCode());
+           updatedCustomer.setCountry(customer.getCountry());
+           repo.save(updatedCustomer);
+       }
+
     }
 
     @DeleteMapping("/customers/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCustomer(@PathVariable int id){
-         repo.deleteById(id);
+        Optional<Customer> returnVal = repo.findById(id);
+        if(returnVal.isPresent()){
+            repo.deleteById(id);
+
+        }
     }
 
 }
---
+++ M6-Challenge-Agogoe-Reuben/customer-data-service/src/test/java/com/company/customerdataservice/controller/CustomerControllerTests.java
@@ -0,0 +1,104 @@
+package com.company.customerdataservice.controller;
+
+import com.company.customerdataservice.model.Customer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+  @SpringBootTest
+  @AutoConfigureMockMvc
+public class CustomerControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+
+    @Test
+    public void shouldReturnCreatedStatus() throws Exception{
+        Customer customer = new Customer("Reuben K", "Agogoe", "ragogoe@princeton.edu",
+                "+16092123905");
+        customer.setAddress1("2004 Frist Campus Center");
+        customer.setCity("Princeton");
+        customer.setCompany("Princeton University");
+        customer.setState("New Jersey");
+        customer.setPostalCode("08540");
+        customer.setCountry("US");
+
+        String inputJson = mapper.writeValueAsString(customer);
+        mockMvc.perform(
+                post("/customers")
+                        .content(inputJson)
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void shouldDeleteCustomerAndReturn204StatusCode() throws Exception{
+        mockMvc.perform(
+                delete("/customers/2")
+        ).andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void shouldReturnCustomersByStateAndOkResponse() throws Exception{
+
+        mockMvc.perform(
+                get("/customers/state/New Jersey")
+        )
+                .andDo(print())
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void shouldReturnCustomerByIdAndOkResponse() throws Exception{
+        mockMvc.perform(
+                get("/customers/2")
+        ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldGetAllCustomers() throws Exception{
+        mockMvc.perform(
+                get("/customers")
+        )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldUpdateWhenInDatabaseAndReturnNoContent() throws Exception{
+        Customer customer = new Customer("Adam", "Johnson", "rjo@gmail.com",
+                "+11111111111");
+        customer.setAddress1("10 Skyport Drive");
+        customer.setCity("San Jose");
+        customer.setCompany("Marriott");
+        customer.setState("California");
+        customer.setPostalCode("33333");
+        customer.setCountry("US");
+
+        String inputJson = mapper.writeValueAsString(customer);
+        mockMvc.perform(
+                put("/customers/1")
+                        .content(inputJson)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andDo(print())
+                .andExpect(status().isNoContent());
+
+    }
+}
